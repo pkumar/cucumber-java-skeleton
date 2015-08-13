625358857b7e2430dac1f5f26e66bc73ac7c6f6d
Pradeep Kumar <Pradeep.Kumar@neustar.biz>
2015-08-13 15:22:35 +0530

    Expanded Stepdefs

--- src/main/java/skeleton/Belly.java
+++ src/main/java/skeleton/Belly.java
@@ -4,4 +4,12 @@ public class Belly {
     public void eat(int cukes) {
 
     }
+    
+    public void wait(int time) {
+    	
+    }
+    
+    public void growl(String arg) {
+    	System.out.println("stop eating cookies" + arg);
+    }
 }
--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,11 +1,26 @@
 package skeleton;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
-    @Given("^I have (\\d+) cukes in my belly$")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
-        Belly belly = new Belly();
-        belly.eat(cukes);
-    }
+	@Given("^I have (\\d+) cukes in my belly$")
+	public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
+		Belly belly = new Belly();
+		belly.eat(cukes);
+	}
+
+	@When("^I wait (\\d+) hour$")
+	public void i_wait_hour(int arg1) throws Throwable {
+		Belly belly = new Belly();
+		belly.wait(arg1);
+	}
+
+	@Then("^my belly should growl as (.*)$")
+	public void my_belly_should_growl(String arg) throws Throwable {
+		Belly belly = new Belly();
+		belly.growl(arg);
+	}
 }
